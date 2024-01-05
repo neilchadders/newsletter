558cb109e4cdd1fa10d4aa8e6342bd27b6e377fd
Neil Chadwick <neilchadders@hotmail.com>
2024-01-05 08:51:21 +0000

    successful link to mysqli

--- subscribe.php
+++ subscribe.php
@@ -12,9 +12,9 @@ $date = date("Y-m-d H:i:s"); // format for date
 
 //echo ($date);
 
-$query = "INSERT INTO $table (first_name, last_name, email, date) VALUES ('$first_name', '$last_name', '$email', '$date')";
+$query = "INSERT INTO $table (first_name, last_name, email, date) VALUES ('$first_name', '$last_name', '$email', '$date')";// $table required from connection.inc.php
 
-mysqli_query($dbc, $query)
-or die("Error inserting email" . mysqli_error($dbc));
+mysqli_query($dbc, $query) || die ("Error inserting email" . mysqli_error($dbc)); // mysqli_query = function that takes following parameters: 1)object from mysqli_connect , in this case $dbc (required from connection.inc.php) 2) query string - see $query 3)
+
 
 echo ' You have been successfully added.';
